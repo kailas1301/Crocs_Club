8d33a2c26b0a88a5314dbb50e2eb46991a87ce1a
unknown <kailaskailu56@gmaol.com>
2024-03-12 13:28:12 +0530

    added splash screen and loading screen

---
+++ lib/presentation/splash_screen/splash_screen.dart
@@ -0,0 +1,37 @@
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:crocs_club/business_logic/Splash/bloc/splash_bloc.dart';
+import 'package:crocs_club/business_logic/Splash/bloc/splash_event.dart';
+import 'package:crocs_club/business_logic/Splash/bloc/splash_state.dart';
+import 'package:crocs_club/presentation/on_boarding/on_boarding.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedSplashScreen(
+      splash: Image.asset('assets/images/logo.png'),
+      splashIconSize: 200,
+      nextScreen: BlocProvider<SplashBloc>(
+        create: (context) => SplashBloc()..add(SetSplash()),
+        child: BlocConsumer<SplashBloc, SplashState>(
+          listener: (context, state) {
+            if (state is SplashLoadedState) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => OnBoardingScreen()),
+              );
+            }
+          },
+          builder: (context, state) {
+            return Container(); // Placeholder widget while waiting for state
+          },
+        ),
+      ),
+      splashTransition: SplashTransition.fadeTransition,
+      duration: 2000, // Adjust duration as needed
+    );
+  }
+}
